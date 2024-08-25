aa1aebafe2789c4fdf4136be51fdb2eb22348144
Andres Sanabria <andrs.dev@gmail.com>
2024-08-25 11:39:52 -0600

    feat: setup prisma with express

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,3 +1,6 @@
+import { PrismaClient, Prisma } from '@repo/db'
+
+const prisma = new PrismaClient()
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
@@ -8,8 +11,29 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (_, res) => {
+app.get('/', async (_, res) => {
   return res.json({ ok: true })
+})
+
+app.get('/users', async (_, res) => {
+  const users = await prisma.user.findMany()
+  return res.json({ users })
+})
+
+app.post('/users', async (req, res) => {
+  const data = req.body
+
+  try {
+    await prisma.user.create({ data })
+    return res.json({ message: 'User Created' })
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log('Message')
+      console.log(e.meta)
+    }
+
+    return res.json({ message: 'User not created' })
+  }
 })
 
 const port = process.env.PORT || 5001
